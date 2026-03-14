d2e800298657183e3fa893d7a8aa5e51629edaf0
sjo <bonzoman@gmail.com>
2026-03-14 13:55:37 +0900

    inittt

--- src/main/java/com/stimi/creator/StimiCreatorApplication.java
+++ src/main/java/com/stimi/creator/StimiCreatorApplication.java
@@ -1,22 +1,16 @@
-package com.bnz.stepmon;
+package com.stimi.creator;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class StimiCreatorApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        // 서버의 모든 시간 기준을 UTC로 강제
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(StimiCreatorApplication.class, args);
     }
 }
